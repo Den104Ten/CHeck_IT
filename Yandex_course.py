19c7a289d86152b42499aad34891f9b1ec949a9f
Den104Ten <dan_eltsov@mail.ru>
2023-01-24 10:47:13 +0300

    Add some

--- Yandex_course.py
+++ Yandex_course.py
@@ -643,14 +643,239 @@ for i in info:
 print(result)"""
 
 
-numbers = [34, 10, 4, 6, 10, 23, 90, 100, 21, 35, 95, 1, 36, 38, 19, 1, 6, 87, 1000, 13456, 360]
+"""numbers = [34, 10, 4, 6, 10, 23, 90, 100, 21, 35, 95, 1, 36, 38, 19, 1, 6, 87, 1000, 13456, 360]
 result = {}
 for i in numbers:
-    for j in range(i + 1):
-        #print(f'{i} % {j + 1} == {i / (j + 1)}')
-        if i % (j + 1) == 0:
-            result.setdefault(i, j)
-print(result)
+    z = []
+    for j in range(1, i + 1):
+        if i % j == 0:
+            z.append(j)
+        else:
+            pass
+    result.setdefault(i, z)
+print(result)"""
+
+
+"""words = ['hello', 'bye', 'yes', 'no', 'python', 'apple', 'maybe', 'stepik', 'beegeek']
+result = {}
+for i in words:
+    z = []
+    for j in i:
+        z.append(ord(j))
+    result.setdefault(i, z)
+print(result)"""
+
+
+"""letters = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J', 10: 'K', 11: 'L', 12: 'M', 13: 'N', 14: 'O', 15: 'P', 16: 'Q', 17: 'R', 18: 'S', 19: 'T', 20: 'U', 21: 'V', 22: 'W', 23: 'X', 24: 'Y', 26: 'Z'}
+
+remove_keys = [1, 5, 7, 12, 17, 19, 21, 24]
+
+result = {}
+for i in letters:
+    if i in remove_keys:
+        pass
+    else:
+        result.setdefault(i, letters[i])
+print(result)"""
+
+
+"""students = {'Timur': (170, 75), 'Ruslan': (180, 105), 'Soltan': (192, 68), 'Roman': (175, 70), 'Madlen': (160, 50), 'Stefani': (165, 70), 'Tom': (190, 90), 'Jerry': (180, 87), 'Anna': (172, 67), 'Scott': (168, 78), 'John': (186, 79), 'Alex': (195, 120), 'Max': (200, 110), 'Barak': (180, 89), 'Donald': (170, 80), 'Rustam': (186, 100), 'Alice': (159, 59), 'Rita': (170, 80), 'Mary': (175, 69), 'Jane': (190, 80)}
+
+result = {}
+for i in students:
+    if students[i][0] > 167 and students[i][1] < 75:
+        result.setdefault(i, students[i])
+print(result)"""
+
+
+"""tuples = [(1, 2, 3), (4, 5, 6), (7, 8, 9), (10, 11, 12), (13, 14, 15), (16, 17, 18), (19, 20, 21), (22, 23, 24), (25, 26, 27), (28, 29, 30), (31, 32, 33), (34, 35, 36)]
+
+result = {}
+for i in tuples:
+    result.setdefault(i[0], i[1:])
+print(result)"""
+
+
+"""student_ids = ['S001', 'S002', 'S003', 'S004', 'S005', 'S006', 'S007', 'S008', 'S009', 'S010', 'S011', 'S012', 'S013']
+student_names = ['Camila Rodriguez', 'Juan Cruz', 'Dan Richards', 'Sam Boyle', 'Batista Cesare', 'Francesco Totti', 'Khalid Hussain', 'Ethan Hawke', 'David Bowman', 'James Milner', 'Michael Owen', 'Gary Oldman', 'Tom Hardy']
+student_grades = [86, 98, 89, 92, 45, 67, 89, 90, 100, 98, 10, 96, 93]
+
+
+result = []
+for key, key_1, value in zip(student_ids, student_names, student_grades):
+    result.append({key: {key_1: value}})
+print(result)"""
 
 
 
+"""import random
+
+num1 = random.randint(0, 17)
+num2 = random.randint(-5, 5)
+print(num1)
+print(num2)"""
+
+
+
+
+"""import random
+
+random.seed(17)   # явно устанавливаем начальное значение для генератора случайных чисел
+
+for _ in range(10):
+    print(random.randint(1, 100))"""
+
+"""import random as r
+print(r.randint(0, 10))"""
+
+"""from random import *
+n = int(input())
+i = 0
+while i != n:
+    rand = randint(1, 2)
+    if rand == 1:
+        print('Орел')
+    else:
+        print('Решка')
+    i += 1"""
+
+
+"""from random import *
+n = int(input())
+i = 0
+while i != n:
+    rand = randint(1, 6)
+    print(rand)
+    i += 1"""
+
+
+"""from random import *
+n = int(input())
+i = 0
+z = []
+while i != n:
+    rand = randint(1, 2)
+    if rand == 1:
+        low_d = randint(65, 90)
+        z.append(chr(low_d))
+    else:
+        up_d = randint(97, 122)
+        z.append(chr(up_d))
+    i += 1
+print(*z, sep='')"""
+
+
+"""from random import *
+i = 0
+z = []
+s = set
+while i != 7:
+    rand = randint(1, 49)
+    if rand in z:
+        pass
+    else:
+        z.append(rand)
+        i += 1
+a = sorted(z)
+print(*a)"""
+
+
+"""from random import *
+def generate_index():
+    up_d = randint(97, 122)
+    up_d2 = randint(97, 122)
+    up_d3 = randint(97, 122)
+    up_d4 = randint(97, 122)
+    print(f'{chr(up_d).upper()}{chr(up_d2).upper()}{randint(0, 99)}_{randint(0, 99)}{chr(up_d3).upper()}{chr(up_d4).upper()}')
+
+generate_index()"""
+
+
+"""from random import *
+matrix = [[1, 2, 3, 4],
+          [5, 6, 7, 8],
+          [9, 10, 11, 12],
+          [13, 14, 15, 16]]
+for i in matrix:
+    shuffle(i)"""
+
+"""from random import *
+for i in range(100):
+    j = 0
+    z = []
+    while j != 7:
+        rand = randint(1, 9)
+        j += 1
+        z.append(rand)
+    print(*z, sep='')"""
+
+
+"""from random import *
+a = str(input())
+z = []
+for i in a:
+    z.append(i)
+shuffle(z)
+print(*z, sep='')"""
+
+
+"""from random import *
+for j in range(5):
+    z = []
+    i = 0
+    while i != 5:
+        rand = randrange(1, 75)
+        if j == 2:
+            if i == 2:
+                z.append(0)
+            else:
+                z.append(rand)
+        else:
+            z.append(rand)
+        i += 1
+    j += 1
+    print(*z)"""
+
+
+"""from random import *
+z = []
+for i in range(1, 76):
+    z.append(i)
+lst = sample(z, 25)
+lst.insert(12, 0)
+print(*lst[:5])
+print(*lst[5:10])
+print(*lst[10:15])
+print(*lst[15:20])
+print(*lst[20:25])"""
+
+
+from random import *
+n = int(input())  # length
+m = int(input())  # count
+for i in range(n):
+    j = 0
+    z = []
+    while j != m:  # Счетчик символов
+        rand = randint(1, 3)  # Если low_d равен l, I, 1, 0, o, O то нужно не добавлять этот символ и пропустить счетчик
+        if rand == 1:
+            up_d = randint(65, 90)
+            res_to_str = chr(up_d)  # ИСПРАВИТЬ НА UP ЕСЛИ НЕ ПРАВИЛЬНО
+            if res_to_str == 'I' or res_to_str == 'O':  # Только большие буквы, без цифр # or O
+                j -= 1
+                pass
+            else:
+                z.append(res_to_str)
+        elif rand == 2:
+            int_d = randint(2, 9)
+            z.append(int_d)
+        else:
+            low_d = randint(97, 122)  # Поменять чтобы исключения не выводились
+            res_to_str_2 = chr(low_d)
+            if res_to_str_2 == 'l' or res_to_str_2 == 'o':
+                j -= 1
+                pass
+            else:
+                z.append(chr(low_d))
+        j += 1
+    print(*z, sep='')
