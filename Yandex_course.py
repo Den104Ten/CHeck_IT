ca87671e8c6d976b5913120f84fe9b74d46f5f2c
dan_e <dan_eltsov@mail.ru>
2023-01-31 22:26:47 +0300

    ok, to pc

--- Yandex_course.py
+++ Yandex_course.py
@@ -598,5 +598,79 @@ positive_numbers = list(filter(func, numbers))  #  преобразуем ите
 
 print(positive_numbers)"""
 
+"""from functools import reduce
+
+numbers = range(10)
+obj = map(lambda x: x + 1, numbers)
+obj = filter(lambda x: x % 2 == 1, obj)
+result = reduce(lambda x, y: x + y, obj, 0)
+
+print(result)
+
+"""
+
+
+"""from functools import reduce
+
+floats = [4.35, 6.09, 3.25, 9.77, 2.16, 8.88, 4.59, 34.23, 12.12, 4.67, 2.45, 9.32]
+words = ['racecar', 'akinremi', 'deed', 'temidayo', 'omoseun', 'civic', 'TATTARRATTAT', 'malayalam', 'nun']
+numbers = [4, 6, 9, 23, 5]
+
+# Исправьте этот код
+map_result = list(map(lambda num: round(num**2, 1), floats))
+filter_result = list(filter(lambda name: name if name == name[::-1] and len(name) > 4 else None, words))
+reduce_result = reduce(lambda num1, num2: num1 * num2, numbers, 1)
+
+print(map_result)
+print(filter_result)
+print(reduce_result)"""
+
+
+from functools import reduce
+
+data = [['Tokyo', 35676000, 'primary'],
+        ['New York', 19354922, 'nan'],
+        ['Mexico City', 19028000, 'primary'],
+        ['Mumbai', 18978000, 'admin'],
+        ['Sao Paulo', 18845000, 'admin'],
+        ['Delhi', 15926000, 'admin'],
+        ['Shanghai', 14987000, 'admin'],
+        ['Kolkata', 14787000, 'admin'],
+        ['Los Angeles', 12815475, 'nan'],
+        ['Dhaka', 12797394, 'primary'],
+        ['Buenos Aires', 12795000, 'primary'],
+        ['Karachi', 12130000, 'admin'],
+        ['Cairo', 11893000, 'primary'],
+        ['Rio de Janeiro', 11748000, 'admin'],
+        ['Osaka', 11294000, 'admin'],
+        ['Beijing', 11106000, 'primary'],
+        ['Manila', 11100000, 'primary'],
+        ['Moscow', 10452000, 'primary'],
+        ['Istanbul', 10061000, 'admin'],
+        ['Paris', 9904000, 'primary']]
+
+"""a = list(map(lambda x: x[1], data))
+print(a)"""
+
+
+"""check10mil = list(map(lambda x: x[0] if x[1] > 10000000 else None, data))
+z = []
+for i in check10mil:
+        z.append(i)
+
+print("Cities"", *z, sep=', ')"""
+
+
+from functools import reduce
+def check10mil(x):
+    z = []
+    for i in x:
+       if i[1] > 10000000:
+           z.append(i[0])
+    print(*z, sep=', ')
+
+
+print("Cities: "), check10mil(data)
+
 
 
