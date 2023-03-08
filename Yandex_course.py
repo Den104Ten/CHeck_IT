a7ff71566332e6b02d6af3d1551525b6b15b992f
dan_e <dan_eltsov@mail.ru>
2023-03-08 23:03:30 +0300

    ok, to pc

--- Yandex_course.py
+++ Yandex_course.py
@@ -1280,10 +1280,28 @@ set_last = set_res.difference(set_general)
 print(*sorted(set_last))"""
 
 
-words = ['Plum', 'Grapefruit', 'apple', 'orange', 'pomegranate', 'Cranberry', 'lime', 'Lemon', 'grapes', 'persimmon', 'tangerine', 'Watermelon', 'currant', 'Almond']
+"""words = ['Plum', 'Grapefruit', 'apple', 'orange', 'pomegranate', 'Cranberry', 'lime', 'Lemon', 'grapes', 'persimmon', 'tangerine', 'Watermelon', 'currant', 'Almond']
 
 set_res = {x[0].lower() for x in words}
+print(*sorted(set_res))"""
+
+
+"""sentence = '''My very photogenic mother died in a freak accident (picnic, lightning) when I was three, and, save for a pocket of warmth in the darkest past, nothing of her subsists within the hollows and dells of memory, over which, if you can still stand my style (I am writing under observation), the sun of my infancy had set: surely, you all know those redolent remnants of day suspended, with the midges, about some hedge in bloom or suddenly entered and traversed by the rambler, at the bottom of a hill, in the summer dusk; a furry warmth, golden midges.'''
+
+set_res = {x.lower().strip(':,.!?();') for x in sentence.split()}
+
+print(*sorted(set_res))"""
+
+
+"""sentence = '''My very photogenic mother died in a freak accident (picnic, lightning) when I was three, and, save for a pocket of warmth in the darkest past, nothing of her subsists within the hollows and dells of memory, over which, if you can still stand my style (I am writing under observation), the sun of my infancy had set: surely, you all know those redolent remnants of day suspended, with the midges, about some hedge in bloom or suddenly entered and traversed by the rambler, at the bottom of a hill, in the summer dusk; a furry warmth, golden midges.'''
+
+set_res = {x.lower().strip(':,.!?();') for x in sentence.split() if len(x.strip(':,.!?();')) < 4}
+
+print(*sorted(set_res))"""
+
+
+files = ['python.png', 'qwerty.py', 'stepik.png', 'beegeek.org', 'windows.pnp', 'pen.txt', 'phone.py', 'book.txT', 'board.pNg', 'keyBoard.jpg', 'Python.PNg', 'apple.jpeg', 'png.png', 'input.tXt', 'split.pop', 'solution.Py', 'stepik.org', 'kotlin.ko', 'github.git']
+
+set_res = {i.lower() for i in files if i[-4:].lower() == '.png'}
+
 print(*sorted(set_res))
-
-
-
